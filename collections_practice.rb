c5b8ae00ece134e27d8e0209717bcb8e74f6a922
Gina Lee <ginalee1114@gmail.com>
2018-07-27 14:56:35 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|element| element.start_with?("r")}
+end 
+
+def contain_a(array)
+  contains_a = []
+  array.map do |element|
+    if (element.include?("a"))
+      contains_a << element
+    end
+  end 
+  contains_a
+end 
+
+def first_wa(array)
+  array.find do |element|
+    element.match(/wa/)
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |element|
+    element.is_a?(String) == false
+  end
+end 
+
+def count_elements(array)
+  
+end 
